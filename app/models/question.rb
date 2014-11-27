4c4769182e982bcf315a0af8c3b5a3a52e47ff6e
Thomas Troelsen <ttro@ucsyd.dk>
2014-11-27 08:32:23 +0100

    Opdatering af interface

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,9 @@ class Question < ActiveRecord::Base
   has_many :answers
   has_many :questions_options
   attr_accessible :header, :question
+
+  def self.correct_answer
+      questions_options.where { |a| a.correct == 1 }
+  end
+
 end
